e520df80dae6aa9e1e6dd92e1aaaa58cd9587f0e
Dominik Picheta <dominikpicheta@googlemail.com>
2014-10-17 18:39:09 +0100

    Remove debug code.

--- cache.nim
+++ cache.nim
@@ -25,10 +25,8 @@ proc invalidateAll*(cache: CacheHolder) =
 template get*(cache: CacheHolder, name: string, grabValue: expr): expr =
   ## Check to see if the cache contains value for ``name``. If it does and the
   ## cache is valid then doesn't recalculate it but returns the cached version.
-  echo(cache.caches)
   mixin normalizePath
   let nName = name.normalizePath()
   if not (cache.caches.hasKey(nName) and cache.caches[nName].valid):
-    echo "Resetting cache."
     cache.caches[nName] = CacheInfo(valid: true, value: grabValue)
   cache.caches[nName].value
