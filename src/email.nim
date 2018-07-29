4dfe95597fe22356c009da9dfef2b9d5f3ed514b
saggit <andtaotao@qq.com>
2018-07-29 21:49:12 +0800

    feat(email): make send mail from address configurable

--- src/email.nim
+++ src/email.nim
@@ -30,7 +30,6 @@ proc rateCheck(mailer: Mailer, address: string): bool =
 proc sendMail(
   mailer: Mailer,
   subject, message, recipient: string,
-  fromAddr = "forum@nim-lang.org",
   otherHeaders:seq[(string, string)] = @[]
 ) {.async.} =
   # Ensure we aren't emailing this address too much.
@@ -50,12 +49,12 @@ proc sendMail(
   let toList = @[recipient]
 
   var headers = otherHeaders
-  headers.add(("From", fromAddr))
+  headers.add(("From", mailer.config.smtpFromAddr))
 
   let encoded = createMessage(subject, message,
       toList, @[], headers)
 
-  await client.sendMail(fromAddr, toList, $encoded)
+  await client.sendMail(mailer.config.smtpFromAddr, toList, $encoded)
 
 proc sendPassReset(mailer: Mailer, email, user, resetUrl: string) {.async.} =
   let message = """Hello $1,
