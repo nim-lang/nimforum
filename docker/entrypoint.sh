030c52020ed04db005c4ae50570b83520c70486e
jiro <jiroron666@gmail.com>
2020-08-23 22:30:06 +0900

    Add docker environment for local development (#257)
    * Add docker environment
    
    * Add document of docker
    
    * Move docker files
    
    * Change context
    
    * Move git submodule command to entrypoint.sh
    
    * Update README

---
+++ docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -eu
+
+git submodule update --init --recursive
+
+# setup
+nimble c -d:release src/setup_nimforum.nim
+./src/setup_nimforum --dev
+
+# build frontend
+nimble c -r src/buildcss
+nimble js -d:release src/frontend/forum.nim
+mkdir -p public/js
+cp src/frontend/forum.js public/js/forum.js
+
+# build backend
+nimble c src/forum.nim
+./src/forum
