08522626cae9711df30dfd0eff596f6ed7a3708d
David Krause <enthus1ast@users.noreply.github.com>
2023-02-24 09:29:17 +0100

    fix db_sqlite import
    Signed-off-by: David Krause <enthus1ast@users.noreply.github.com>

--- src/sitemap.nim
+++ src/sitemap.nim
@@ -1,4 +1,9 @@
-import simpleSitemap, db_sqlite, times, os, uri
+import simpleSitemap, times, os, uri
+
+when NimMajor >= 1 and NimMinor >= 9:
+  import db_connector/db_sqlite
+else:
+  import std/db_sqlite
 
 type
   SitemapGenerator* = object
