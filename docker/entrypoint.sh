007a2867a5e670f203f3ba3549cdfc5396435160
aethrvmn <me@aethrvmn.gr>
2026-07-03 09:24:04 +0000

    updated docker from nim 1.6.4 to 2.2.4 (#389)
    - Updated docker image to `2.2.4-ubuntu-regular`
    - Removed deprecated `version` tag from `docker-compose.yml`
    - Converted `nimble c` calls to `nim c`, and added `--mm:refc` of final binary, because it gets segfault using ORC

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,15 +5,15 @@ set -eu
 git submodule update --init --recursive
 
 # setup
-nimble c -d:release src/setup_nimforum.nim
+nim c -d:release src/setup_nimforum.nim
 ./src/setup_nimforum --dev
 
 # build frontend
-nimble c -r src/buildcss
-nimble js -d:release src/frontend/forum.nim
+nim c -r src/buildcss
+nim js -d:release src/frontend/forum.nim
 mkdir -p public/js
 cp src/frontend/forum.js public/js/forum.js
 
 # build backend
-nimble c src/forum.nim
+nim c -mm:refc src/forum.nim
 ./src/forum
